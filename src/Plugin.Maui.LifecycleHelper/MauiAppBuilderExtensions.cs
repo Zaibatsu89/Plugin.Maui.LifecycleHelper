95a7fc7455c10277c2460bb421e87eeb058318ea
Rinse Cramer <Zaibatsu89@users.noreply.github.com>
2024-05-04 11:50:46 +0200

    Create MauiAppBuilderExtensions.cs

---
+++ src/Plugin.Maui.LifecycleHelper/MauiAppBuilderExtensions.cs
@@ -0,0 +1,17 @@
+namespace Plugin.Maui.LifecycleHelper;
+
+public static class MauiAppBuilderExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="ILifecycleHelper"/> as a singleton to the dependency injection container.
+    /// </summary>
+    /// <param name="mauiAppBuilder">The <see cref="MauiAppBuilder"/> to register the package with.</param>
+    /// <returns>The <paramref name="mauiAppBuilder"/> supplied in order to allow for chaining of method calls.</returns>
+    public static MauiAppBuilder AddLifecycleHelper(
+        this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton(LifecycleHelper.Current);
+
+        return mauiAppBuilder;
+    }
+}
